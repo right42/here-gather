2590780d02cd949cff5635d7cbc8e0950fa3bec4
right42 <chjm452@naver.com>
2021-05-18 15:49:40 +0900

    project init

---
+++ src/main/java/me/right42/heregather/HeregatherApplication.java
@@ -0,0 +1,13 @@
+package me.right42.heregather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeregatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeregatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/right42/heregather/HeregatherApplicationTests.java
@@ -0,0 +1,13 @@
+package me.right42.heregather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeregatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
