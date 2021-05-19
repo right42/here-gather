ac510086eac1836cd603942bc58794d1d75c2f81
right42 <chjm452@naver.com>
2021-05-19 18:51:57 +0900

    user entity 생성

---
+++ src/test/java/me/right42/heregather/domain/user/UserTest.java
@@ -0,0 +1,18 @@
+package me.right42.heregather.domain.user;
+
+import static org.assertj.core.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+class UserTest {
+
+	@Test
+	void 생성테스트(){
+		User user = User.builder().build();
+
+		assertThat(user).isNotNull();
+	}
+
+}
